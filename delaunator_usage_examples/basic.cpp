e364e7a94127235685e66ff692ccf18a87eca8cb
Elizabeth <meerajees@gmail.com>
2025-03-09 14:49:13 +0530

    delaunator library and examples added

---
+++ delaunator_usage_examples/basic.cpp
@@ -0,0 +1,22 @@
+#include "../include/delaunator.hpp"
+#include <cstdio>
+
+int main() {
+    /* x0, y0, x1, y1, ... */
+    std::vector<double> coords = {-1, 1, 1, 1, 1, -1, -1, -1};
+
+    //triangulation happens here
+    delaunator::Delaunator d(coords);
+    printf("%ld  %ld  %ld\n\n",d.triangles[0], d.triangles[1], d.triangles[2]);
+    for(std::size_t i = 0; i < d.triangles.size(); i+=3) {
+        printf(
+            "Triangle points: [[%f, %f], [%f, %f], [%f, %f]]\n",
+            d.coords[2 * d.triangles[i]],        //tx0
+            d.coords[2 * d.triangles[i] + 1],    //ty0
+            d.coords[2 * d.triangles[i + 1]],    //tx1
+            d.coords[2 * d.triangles[i + 1] + 1],//ty1
+            d.coords[2 * d.triangles[i + 2]],    //tx2
+            d.coords[2 * d.triangles[i + 2] + 1] //ty2
+        );
+    }
+}
